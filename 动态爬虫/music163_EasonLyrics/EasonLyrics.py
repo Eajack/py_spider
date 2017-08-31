50cff909fc97de2e11483a3d90e7573a3b1a9c60
Eajack Lau <eajacklau@163.com>
2017-08-31 20:20:59 +0800

    Eason网易云歌词爬虫 + 文本分析
    Eason网易云歌词爬虫 + 文本分析

---
+++ 动态爬虫/music163_EasonLyrics/EasonLyrics.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python3
+# -*- coding : utf-8 -*-
+#==================================================================================================================
+# Author: Eajack
+# date:2017/8/23 - 2017/8/26
+#==================================================================================================================
+# Function：
+#   1- 爬取网易云Eason所有歌曲歌词储存为txt
+#   2-
+#==================================================================================================================
+# keyPoints:
+#   1- 网易云音乐的html中frame套frame，需要用selenium转换frame才能找到元素，bs不行
+#       driver.switch_to.frame(driver.find_element_by_xpath("//iframe"))
+#   2- 经典的 utf-8(unicode) => gbk 编码问题，有两个问题
+#       1)- windows下文件标题不能有 “/ ” “\”等标点符号
+#       解决:fileName = fileName.replace("\xa0", " ")
+#            fileName = fileName.replace("/", " ")
+#            fileName = fileName.replace(" ", "")。记得加上 “ fileName = ”
+#       2)- windows下txt文件内容编码默认为gbk
+#       解决：txtFile = open(fileName,'w',encoding='utf-8')。改编码方式为utf-8
+#             也可以顺便lyric = lyric.replace(u"\xa0",u" ")以备以后读取编码问题
+#==================================================================================================================
+# Console:
+# 没有歌词的歌曲：
+#      ['L.I.F.E.Overture.txt', '那一夜没有雪.txt', '孤儿仔.txt', '想哭.txt', 'MusicOnly.txt', '黎喇.txt',
+#       'EasonsAngel.txt', '27Seconds.txt']
+# 下载失败的歌曲: []
+# 总共大约用时30分钟
+#==================================================================================================================
+# Attention：
+#   Medley文件需删去
+#==================================================================================================================
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import requests
+import json
+import os
+import re
+import time
+# selenium库
+from selenium import webdriver
+
+# global
+## 网易云请求头
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Cookie': '_ntes_nnid=e76639646686a8ddf4dbd9f865dbfe4e,1503039315399; _ntes_nuid=e76639646686a8ddf4dbd9f865dbfe4e; MUSIC_A=2ef1d1bf10e0970aa8ef2a400e11078ecfd091512723c7fb5f9d567371dac57a16f827d623128710018b9570d66b88bfb23ea72ab5261c23fb97b0129e36bdf9f546586f7bb9e6ae8328c980cd21a668; os=uwp; osver=10.0.14393.447; appver=1.3.3; deviceId=0d56743a3d5158a11f5a1fbf3a7dc746; FromPlatform=uwp; playerid=74378889; __csrf=87d059fd2c8ddc7e9cf4110cb944c364; JSESSIONID-WYYY=eXN26albKtQloWhh8EfQV77Qw519A%5Cl7vzcIjnvdMC%5CSufYdMklqQW%5C4vvSVi%5Cr0HWuK8hlb1g35D4IMcEC0h%5C6hl%5C14iF4XK5Wr%5CWflTCSH0p%2FbekQt4CuC%5CvXbN43GwErri0Brp%5CFipMllOCmfBZxiDq9qCJgkQOlsNGAkc%5CqRVGJu%3A1503483000489; _iuqxldmzr_=32',
+    'DNT': '1',
+    'Host': 'music.163.com',
+    'Pragma': 'no-cache',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+}
+
+## 专辑ID统计
+albumIDList = []
+## key:歌名,value:ID
+songName2ID_dict = {}
+## 下载失败歌名
+downloadFailedSong = []
+## 没有歌词的歌名
+noLyricsSong = []
+
+# 获取Eason 所有专辑ID
+def getAlbumIDList(singerID, limit):
+    '''
+    Function：
+        1- 获取Eason 所有专辑ID
+    :param singerID: 2116
+    :param limit: 12
+  
+    :return: albumIDList => list
+    '''
+    global albumIDList
+
+    subAlbumLink = r'https://music.163.com/#/artist/album'
+
+    # 由于需要转移frame，需要selenium
+    for offset in range(0, 108, 12):
+        # selenium部分
+        # albumLink eg: https://music.163.com/#/artist/album?id=2116&limit=12&offset=0
+        albumLink = subAlbumLink + r"?id=" + str(singerID) + r"&limit=" + str(limit) + r"&offset=" + str(offset)
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(10)  # 隐式等待
+        driver.get(albumLink)
+        time.sleep(5)  # 等待
+        driver.switch_to.frame(driver.find_element_by_xpath("//iframe"))  # 关键，转换frame
+        html = BeautifulSoup(driver.page_source, "lxml")
+
+        # soup页面解析，获取当前页面所有歌曲ID
+        for albumIDtag in html.findAll("a", {"href": re.compile("^\/album\?id\=.*")}):
+            albumID = albumIDtag["href"].replace("/album?id=", "")
+            if albumID not in albumIDList:
+                albumIDList.append(albumID)
+
+        # 浏览器关闭
+        driver.quit()
+
+# 获取Eason所有专辑内单曲歌曲ID以及所有歌名，过滤同样歌名歌曲
+def getSongIDandsongNameList():
+    '''
+    Function:
+        1- 获取Eason所有专辑内单曲歌曲ID以及所有歌名
+        2- 过滤同一首歌
+    :return: songName2ID_dict[songName] = songID
+    '''
+    global albumIDList,songName2ID_dict
+
+    subInnerAlbumLink = r'https://music.163.com/#/album'
+    haveName = False
+
+    # 由于需要转移frame，需要selenium
+    for albumID in albumIDList:  # 根据网址总结规律
+        # selenium部分
+        # InnerAlbumLink eg: https://music.163.com/#/album?id=35835294
+        albumLink = subInnerAlbumLink + r"?id=" + str(albumID)
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(10)  # 隐式等待
+        driver.get(albumLink)
+        time.sleep(5)  # 等待
+        driver.switch_to.frame(driver.find_element_by_xpath("//iframe"))  # 关键，转换frame
+        html = BeautifulSoup(driver.page_source, "lxml")
+
+        # soup页面解析，获取当前页面所有歌曲ID
+        for songIDtag in html.findAll("a", {"href": re.compile("^\/song\?id\=.*")}):
+            songID = songIDtag["href"].replace("/song?id=", "")
+            songName = str(songIDtag.b["title"])
+
+            ## songName 去重处理 & 字符串处理
+            index1 = songName.find("-")
+            index2 = songName.find("(")
+            if (index1 != -1):
+                songName = songName[0:index1]
+            if (index2 != -1 and index2 != 0):
+                songName = songName[0:index2]
+            elif(index2 == 0):
+                index3 = songName.find("(",index2+1)
+                songName = songName[0:index3]
+            songName = songName.strip()
+
+            ## 空列表处理，直接赋值
+            if(len(songName2ID_dict) == 0):
+                songName2ID_dict[songName] = songID
+
+                #debug use
+                print(songID, songName)
+
+                continue
+
+            ## 判断songNameList中是否有同一首歌曲
+            for currentsongName in songName2ID_dict.keys():
+              ## 存在歌曲了，包括剔除live版本等其它版本，因为歌词一样
+              if (currentsongName == songName):
+                haveName = True
+                break
+              else:
+                haveName = False
+
+            ## 判断有无歌曲后操作
+            if (haveName):
+              continue
+            else:
+              if (songID not in songName2ID_dict.values()):
+                songName2ID_dict[songName] = songID
+                ##debug use
+                print(songID,songName)
+
+        # 浏览器关闭
+        driver.quit()
+
+# 获取歌词，储存为txt文件
+def getLyric():
+    '''
+    Function:
+        1- 获取歌词
+        2- 并储存为txt文件
+    :param:
+        songName2ID_dict => dict
+            songName2ID_dict[songName] = songID
+    :return: txt歌词文档
+    '''
+    global headers,songName2ID_dict,downloadFailedSong,noLyricsSong
+
+    for songName,songID in songName2ID_dict.items():
+        ## 0- 判断文件是否存在
+        ## 转移目录
+        lyricPath = r"D:\STUDYING\MyProjects\pycharm\music163_EasonLyrics\Lyrics"
+        os.chdir(lyricPath)
+        ## 文件名
+        fileName = songName + ".txt"
+        ## 关键点:\xa0 编码问题，记得除了“fileName.replace("\xa0", " ")”，还有 “fileName =”
+        fileName = fileName.replace("\xa0", " ")
+        fileName = fileName.replace("/", " ")
+        fileName = fileName.replace(" ", "")
+        ## 判断文件是否存在
+        if os.path.exists(fileName):
+            continue
+
+        # 1-爬取歌词
+        ## 网页解析，关键在于songID
+        lyricUrl = r'http://music.163.com/api/song/lyric?' + 'id=' + str(songID) + r'&lv=1&kv=1&tv=-1'
+        request = requests.get(lyricUrl, headers=headers)
+        request.encoding = 'utf-8'
+        lyric_json = request.text
+        ## 读取JSON文件
+        lyric_dict = json.loads(lyric_json)
+
+        try:# 歌曲没有歌词
+            lyric = lyric_dict["lrc"]["lyric"]
+            ## 正则去掉 [00:00.00] 类似
+            extra = re.compile("\[.*\]")
+            lyric = re.sub(extra, "", lyric)
+            lyric = lyric.strip()
+        except KeyError as e:
+            noLyricsSong.append(fileName)
+            continue
+
+        # 2-储存为txt文件
+        ## 储存为txt文件
+        try:
+            txtFile = open(fileName,'w',encoding='utf-8')
+            lyric = lyric.replace(u"\xa0",u" ")
+            txtFile.write(lyric)
+            txtFile.close()
+            print(songName + "\t已经下载完歌词啦")
+        except UnicodeEncodeError:
+            downloadFailedSong.append(songName)
+
+        # # debug use
+        # print(lyric)
+
+if __name__ == '__main__':
+    start = time.time()
+
+    # 1- 先获取Eason所有专辑ID
+    singerID = 2116
+    limit = 12
+    getAlbumIDList(singerID, limit)
+    # 2- 遍历所有专辑ID获取所有歌曲ID & 所有歌名
+    getSongIDandsongNameList()
+    # 3- 遍历所有歌曲ID获取歌词
+    getLyric()
+    ## 没歌词的歌以及下载失败的歌
+    print("没有歌词的歌曲：")
+    print(noLyricsSong)
+    print("下载失败的歌曲:")
+    print(downloadFailedSong)
+
+    end = time.time()
+    print("总共大约用时%d分钟"%((end - start) // 60) )
---
+++ 动态爬虫/music163_EasonLyrics/wordsAnalysis.py
@@ -0,0 +1,300 @@
+# -*- coding : utf-8 -*-
+#!/usr/bin/env python3
+# ===============================================================================
+# Author: Eajack
+# date:2017/8/27 - 2017/8/
+# ===============================================================================
+# Function：歌词文本分析
+#   1- jieba分词，去单字
+#   2- 根据词频生成词云
+#   3- 词频统计，做柱形图 & 饼状图
+#   4- 歌词情绪分析简易版demo (饼状图 & )
+# ===============================================================================
+# keyPoints:
+#   1- 多百度，google & github
+# ===============================================================================
+
+import jieba
+import os
+from wordcloud import WordCloud,ImageColorGenerator
+from scipy.misc import imread
+from os import path
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+from snownlp import SnowNLP
+
+# 遍历读取歌词文件，jieba分词
+def jieba_split():
+    '''
+    遍历读取歌词文件，jieba分词
+    :return: 
+    '''
+    global wordList
+    ## 文件目录
+    lyricsPath = r'D:\STUDYING\MyProjects\pycharm\music163_EasonLyrics\Lyrics'
+    os.chdir(lyricsPath)
+    lyricstxtList = [txtfile for txtfile in os.listdir('.')]
+    ## print(lyricstxtList)
+
+    for fileName in lyricstxtList:
+        txtFile = open(fileName,'r',encoding='utf-8')
+        for line in txtFile:
+            flag = ("作曲" in line) or ("作词" in line) or ("编曲" in line)
+            if (flag):
+                continue
+            else:
+                ## 判断是否为英文
+                lineBuffer = line.replace(" ","")
+                if lineBuffer.isalpha():
+                    ## 英文直接按照空格分词
+                    Engseg_list = line.split()
+                    for seg in Engseg_list:
+                        wordList.append(seg)
+
+                Chiseg_list = jieba.cut(line)
+                for seg in Chiseg_list:
+                    ## 判断是否为中文
+                    if (u'\u4e00' <= seg <= u'\u9fff'):
+                        ### 去单字
+                        code = str(seg.encode('UTF-8'))
+                        isOnehanzi = ( code.count("\\x") == 3 )
+                        if isOnehanzi:
+                            continue
+                    ## 判断是否为英文
+                    isEngwords = (seg.isalpha())
+
+                    ## 1-中文非单字 & 2- 英文单词
+                    if ( not isOnehanzi and isEngwords ):
+                        wordList.append(seg)
+        txtFile.close()
+
+# 绘制词云图
+def draw_wordCloud():
+    '''
+    画出词云图
+    :return: 
+    '''
+    ## 读取wordList，转化为str
+    global wordList
+    cut_text = ""
+    for word in wordList:
+        cut_text = cut_text + word + " "
+
+    ## 生成词云
+    os.chdir(r"D:\STUDYING\MyProjects\pycharm\music163_EasonLyrics")
+    d = path.dirname(__file__)  # 当前文件文件夹所在目录
+    color_mask = imread("Eason.jpg")  # 读取背景图片
+    plt.imshow(color_mask)
+
+    cloud = WordCloud(
+        font_path=path.join(d, 'simsun.ttc'),
+        background_color='white',
+        mask=color_mask,
+        max_words=2000,
+        max_font_size=40,
+    )
+    word_cloud = cloud.generate(cut_text)  # 产生词云
+
+    ## show
+    plt.imshow(word_cloud, interpolation="bilinear")
+    plt.axis('off')
+    plt.show()
+
+# 统计词频，画柱形图、饼状图
+def caculateWordFrequency():
+    '''
+    统计词频，画柱形图、饼状图
+    :return: 
+    '''
+    global wordList,wordFrequency
+    ## 统计词频，储存在wordFrequency dict里面
+    for word in wordList:
+        if word in wordFrequency.keys():
+            wordFrequency[word] += 1
+        else:
+            wordFrequency[word] = 1
+    ## 按value值排序
+    items = wordFrequency.items()
+    sortedItems = [ [item[1],item[0]] for item in items ]
+    sortedItems.sort()  ## 顺序
+    sortedItems.reverse()      ## 降序
+
+    ## 取前30个，画柱形图
+    font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)    ##字体设置
+    X_labels = [ sortedItems[i][1] for i in range(0,30) ]
+    Y = [ sortedItems[i][0] for i in range(0,30) ]
+    X = range(len(X_labels))
+    fig = plt.figure()
+    plt.bar(X, Y, color="green")
+    plt.xticks(X, X_labels,fontproperties=font)
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.title("TOP 30th words")
+    plt.show()
+
+    ## 取前30个，画饼状图
+    labels = [ sortedItems[i][1] for i in range(0,len(sortedItems)) ]
+    ### 求比例
+    totalNum = 0
+    rates = []
+    for sortedItem in sortedItems:
+        totalNum += sortedItem[0]
+    for i in range(0,len(sortedItems)):
+        rate = sortedItems[i][0] / totalNum * 100
+        rate = round(rate,10)
+        rates.append(rate)
+    ### 取前30个词语
+    labels = labels[0:30]
+    rates = rates[0:30]
+    ### 作图
+    explode = [0] * 30
+    explode[0] = explode[1] = 0.1   ## 突出第一、二个
+    fig = plt.figure()
+    plt.axes(aspect=1)
+    patches,l_text,p_text = plt.pie(
+        x=rates,
+        labels=labels,
+        explode=explode,
+        autopct='%3.1f %%',
+        shadow=True,
+        labeldistance=1.1,
+        startangle=90,
+        pctdistance=0.8
+    )
+    ### 中文显示问题
+    for font in l_text:
+        font.set_fontproperties(FontProperties(fname=r"c:\windows\fonts\simsun.ttc"))
+    plt.show()
+
+# 歌词情绪分析
+def motionAnalyze():
+    '''
+    思路：
+        1- 遍历读取所有歌词文本，储存每一首歌歌词文档为lyricsList
+        2- 利用 SnowNLP给当前文档评分
+        3- 统计所有文档评分dict形式储存
+        4- 可视化
+    :return: 
+    '''
+    motionGrade = {}
+    ## 文件目录
+    lyricsPath = r'D:\STUDYING\MyProjects\pycharm\music163_EasonLyrics\Lyrics'
+    os.chdir(lyricsPath)
+    lyricstxtList = [txtfile for txtfile in os.listdir('.')]
+
+    for fileName in lyricstxtList:
+        ## 读取本文档，储存在currentLyrics字符串
+        txtFile = open(fileName, 'r', encoding='utf-8')
+        currentLyrics = ""
+        for line in txtFile:
+            currentLyrics = currentLyrics + line
+
+        ## SnowNLP使用
+        ### 先对每个歌词文本分句，评价每句的情感值，求平均
+        currentLyrics = currentLyrics.replace("\n", "。")
+        currentLyrics = currentLyrics.replace(" ", "")
+        text = SnowNLP(currentLyrics)
+        grade = 0;
+        ### 获得所有句子评分总和以及平均值
+        for sentence in text.sentences:
+            sentenceSNOW = SnowNLP(sentence)
+            grade += sentenceSNOW.sentiments
+        grade = grade / len(text.sentences)     ### 求均值
+
+        ## 储存情感值
+        fileName = fileName.replace(".txt","")
+        motionGrade[fileName] = round(grade,5)
+        print(fileName,"已经读取完")
+
+    ## 情感分布饼状图
+    ### 定义 grade >= 0.6 属于正面情绪，<= 0.6为负面情绪
+    positiveNum = 0
+    negativeNum = 0
+    totalNum = 0
+    for grade in motionGrade.values():
+        if (grade >= 0.6):
+            positiveNum += 1
+        else:
+            negativeNum += 1
+        totalNum += 1
+    print(positiveNum,negativeNum,totalNum)
+    ### 画饼状图
+    rates = [ round( ((float)(positiveNum / totalNum)),2 ),
+              round(((float)(negativeNum / totalNum)), 2)]
+    labels = [u"正面情绪",u"负面情绪"]
+    explode = [0] * 2
+    fig = plt.figure()
+    plt.axes(aspect=1)
+    patches, l_text, p_text = plt.pie(
+        x=rates,
+        labels=labels,
+        explode=explode,
+        autopct='%3.1f %%',
+        labeldistance=1.1,
+        startangle=90,
+        pctdistance=0.8
+    )
+    ### 中文显示问题
+    for font in l_text:
+        font.set_fontproperties(FontProperties(fname=r"c:\windows\fonts\simsun.ttc"))
+    plt.show()
+
+    ## 情感分布折线图
+    ## X-得分区间  Y- 该区间歌曲个数
+    ### 确定区间，统计区间歌曲数量
+    X = range(1,11,1)     ### 1 => 0~0.1区间，2 => 0.1~0.2区间 ……
+    Y = [0] * 10
+    for grade in motionGrade.values():
+        ### 暴力简单分区……
+        if ( 0 <= grade < 0.1 ):
+            Y[0] += 1
+        elif ( 0.1 <= grade < 0.2 ):
+            Y[1] += 1
+        elif ( 0.2 <= grade < 0.3 ):
+            Y[2] += 1
+        elif ( 0.3 <= grade < 0.4 ):
+            Y[3] += 1
+        elif ( 0.4 <= grade < 0.5 ):
+            Y[4] += 1
+        elif ( 0.5 <= grade < 0.6 ):
+            Y[5] += 1
+        elif ( 0.6 <= grade < 0.7 ):
+            Y[6] += 1
+        elif ( 0.7 <= grade < 0.8 ):
+            Y[7] += 1
+        elif ( 0.8 <= grade < 0.9 ):
+            Y[8] += 1
+        elif ( 0.9 <= grade < 1.0 ):
+            Y[9] += 1
+
+    ### 画折线图
+    fonts = {'fontname': 'SimHei', 'size': '20'}
+    fig1 = plt.figure()
+    plt.plot(X, Y,\
+             linewidth=3,\
+             color='r',\
+             marker='o',\
+             markerfacecolor='blue',\
+             markersize=5\
+    )
+    plt.axis([0,11,0,250])
+    plt.title(u'情感指数曲线',**fonts)
+    plt.xlabel(u'情感指数区间',**fonts)
+    plt.ylabel(u'歌曲数量',**fonts)
+    plt.text(1, 100, "PS:横坐标中，1 => 0.1-0.2区间，2 => 0.2-0.3区间etc.。", \
+                size=15, alpha=1.0,family='SimHei')
+    plt.grid(X)
+    plt.show()
+
+if __name__ == "__main__":
+    # global
+    ## 所有分词list
+    wordList = []
+    ## 词频dict,wordFrequency[word] = num
+    wordFrequency = {}
+
+    # game start
+    jieba_split()
+    draw_wordCloud()
+    caculateWordFrequency()
+    motionAnalyze()
